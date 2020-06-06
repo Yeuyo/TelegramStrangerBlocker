815af14bd4c239dde4e0a38d58a3bb25c634521d
Yeuyo <38599824+Yeuyo@users.noreply.github.com>
2020-06-06 08:09:40 +0100

    Update AutoBlockStranger.py
    Moved number of pinned chat out of body so don't need to be redefined multiple times, relying on Telethon's mutual_contact instead of a safe list.

--- AutoBlockStranger.py
+++ AutoBlockStranger.py
@@ -7,7 +7,8 @@ phone = '[with country code]'
 session_file = '[@id]' #'/path/to/session/file'  # use your username if unsure
 #password = 'YOUR_PASSWORD'  # if you have two-step verification enabled
 
-safeusers="Yew Yan Wong"
+#safeusers="Yew Yan Wong" #if you want to add another list of users beside mutual contacts to allow list
+number_of_pinned_chat=5
 # content of the automatic reply
 message = "```You have messaged Yeuyo without prior agreement, this chat has been archived and will be deleted unless a reason is given.```"
 
@@ -21,15 +22,15 @@ if __name__ == '__main__':
     async def handle_new_message(event):
         if event.is_private:  # only auto-reply to private chats
             from_ = await event.client.get_entity(event.from_id)  # this lookup will be cached by telethon
-            if not from_.bot:  # don't auto-reply to bots
+            if not from_.bot and not from_.mutual_contact:  # don't auto-reply to bots and mutual contacts
                 dialogs = await client.get_dialogs()
-                first = dialogs[5] #replace 5 with your number of pinned messages
+                first = dialogs[number_of_pinned_chat]
                 #print(first.title)
-            if first.title not in safeusers:
+            #if first.title not in safeusers: #if you want to add another list of users beside mutual contacts to allow list
                 await event.respond(message)
                 #await client.send_file(first.id, 'C:/image.jpg') #uncomment to reply with a photo
-                await client.edit_folder(dialogs[5], 1) #add chat to archive, 0 to unarchive
-                #await client.delete_dialog(dialogs[5]) #delete chat number 6
+                await client.edit_folder(dialogs[number_of_pinned_chat], 1) #add chat to archive, 0 to unarchive
+                #await client.delete_dialog(dialogs[number_of_pinned_chat]) #delete chat
 
                 #print(time.asctime(), '-', event.message)  # optionally log time and message
                 #time.sleep(1)  # pause for 1 second to rate-limit automatic replies
